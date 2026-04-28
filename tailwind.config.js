2a9ce6fbb14af43a8ba66baa89559639ed72caea
Yupi <youngperchik2404@gmail.com>
2026-04-28 16:15:13 +0500

    change styles to win95 esthetics

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
     "./src/**/*.{ts,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'win-gray': '#c0c0c0',
+        'win-blue': '#000080',
+        'win-blue-light': '#1084d0',
+        'win-border-light': '#ffffff',
+        'win-border-dark': '#808080',
+      },
+      boxShadow: {
+        'win-outset': 'inset 1px 1px #fff, inset -1px -1px #808080, 1px 1px #000',
+        'win-inset': 'inset 1px 1px #808080, inset -1px -1px #fff, 1px 1px #dfdfdf',
+      }
+    },
   },
   plugins: [],
 }
-
